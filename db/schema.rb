58cc4919b0605169e282a8ca8cc22b75a09da9bb
Tamara Temple <tamouse@gmail.com>
2014-10-07 05:25:41 +0000

    first

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141007052057) do
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "city"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "temp_data", force: true do |t|
+    t.string   "this"
+    t.string   "that"
+    t.string   "the_other"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "location_id"
+  end
+
+  add_index "temp_data", ["location_id"], name: "index_temp_data_on_location_id"
+
+end
